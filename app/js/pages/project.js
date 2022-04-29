54fa766e196c3e5ad52d1f3d7311be08838a730e
Egor1496 <Egor1496Work@mail.ru>
2022-04-29 13:00:45 +0300

    0.0.9

---
+++ app/js/pages/project.js
@@ -0,0 +1,38 @@
+$(document).ready(() => {
+	Fancybox.bind('[data-fancybox="gallery"]', {
+		dragToClose: false,
+		Toolbar: false,
+		closeButton: "top",
+		Image: {
+			zoom: false,
+		},
+		on: {
+			initCarousel: (fancybox) => {
+				const slide = fancybox.Carousel.slides[fancybox.Carousel.page];
+				fancybox.$container.style.setProperty("--bg-image", `url("${slide.$thumb.src}")`);
+			},
+			"Carousel.change": (fancybox, carousel, to, from) => {
+				const slide = carousel.slides[to];
+				fancybox.$container.style.setProperty("--bg-image", `url("${slide.$thumb.src}")`);
+			},
+		},
+	});
+	Fancybox.bind('[data-fancybox="gallery2"]', {
+		dragToClose: false,
+		Toolbar: false,
+		closeButton: "top",
+		Image: {
+			zoom: false,
+		},
+		on: {
+			initCarousel: (fancybox) => {
+				const slide = fancybox.Carousel.slides[fancybox.Carousel.page];
+				fancybox.$container.style.setProperty("--bg-image", `url("${slide.$thumb.src}")`);
+			},
+			"Carousel.change": (fancybox, carousel, to, from) => {
+				const slide = carousel.slides[to];
+				fancybox.$container.style.setProperty("--bg-image", `url("${slide.$thumb.src}")`);
+			},
+		},
+	});
+});
