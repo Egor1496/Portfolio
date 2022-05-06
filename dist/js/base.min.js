f8bda933dbb7f0a20f1577ddb50e22640cb14fbf
Egor1496 <Egor1496Work@mail.ru>
2022-05-06 17:35:27 +0300

    0.1.4

--- dist/js/base.min.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(() => {
-	$("head").append("<style> :root{" + colorThemeList[getRandomInt(0, 5)].join(";") + "}</style>");
-});
-
-function getRandomInt(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
--- dist/js/main.min.js
+++ dist/js/main.min.js
@@ -72,3 +72,13 @@ var colorThemeList = [
 		"--button-radius: 20px",
 	],
 ];
+
+$(document).ready(() => {
+	$("head").append("<style> :root{" + colorThemeList[getRandomInt(0, 5)].join(";") + "}</style>");
+});
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
